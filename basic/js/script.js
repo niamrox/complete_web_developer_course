3d0e4ed1db3c52898e97a4e3a269b496975a006d
Niamul Hasan <niamul111@gmail.com>
2015-09-27 20:32:20 +0600

    Lecture 99

--- basic/js/script.js
+++ basic/js/script.js
@@ -1,3 +1,15 @@
-/**
- * Created by niamul on 6/13/15.
- */
+var car = {type: "Fiat", model: 500, color: "white"};
+function person(firstName,lastName,id){
+    this.fistName=firstName;
+    this.lastName=lastName;
+    this.id=id;
+    this.fullName=function(){
+        return this.fistName + " " + this.lastName;
+    }
+}
+
+var niam= new person("Niamul","Hasan",555);
+
+var tuhin=new person("Name2","Name3",666)
+
+document.getElementById("demo").innerHTML=tuhin.fullName();
