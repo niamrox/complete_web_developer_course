d9d46563ccb4e71227a7172b2f4b674a30c77dab
Niamul Hasan <niamul111@gmail.com>
2015-09-28 03:03:15 +0600

    Lecture 100

--- basic/js/script.js
+++ basic/js/script.js
@@ -1,3 +1,16 @@
 /**
  * Created by niamul on 6/13/15.
  */
+//toggle navigation
+
+    $('.btn').click(function(){
+        if($('.more').hasClass('visible')){
+            $('.more').removeClass('visible');
+            $('.more').addClass('hidden');
+
+        } else {
+            $('.more').removeClass('hidden');
+            $('.more').addClass('visible');
+        }
+        return false;
+    });
